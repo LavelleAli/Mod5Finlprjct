0f365efcbe5daaff1f38497d42518f7f01cf1309
Lavelle_Ali <lavelleali7@gmail.com>
2026-02-08 15:57:48 -0500

    Latest Update

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-/ http://www.omdbapi.com/?i=tt3896198&apikey=e8773e4
+// API Key: http://www.omdbapi.com/?i=tt3896198&apikey=e8773e4
+// Data request are to be sent to: http://www.omdbapi.com/?apikey=[yourkey]&
+// Data Request API Key format : e8773e4 then you put the "&" symbol then the "=" symbol then "fast"
+
+
+
+async function getAPI() {
+    const movie = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=e8773e4');
+    const movieData = await movie.json();
+    console.log(movieData)
+    movieData.map()
+
+    
+}
+
+getAPI()
+
+
