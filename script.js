d279c418d7366babf53487cff2c68606dde27ab8
Lavelle_Ali <lavelleali7@gmail.com>
2026-02-20 14:56:02 -0500

    Lets Try Again

--- script.js
+++ script.js
@@ -6,8 +6,6 @@
 
 const movieListEl = document.querySelector('.movies');
 
-
-
 // This function translates the API to HTML and puts it on the page
 async function getAPI() {
     const getMovies = await fetch('http://www.omdbapi.com/?apikey=e8773e4&s=Avengers')
@@ -18,6 +16,7 @@ async function getAPI() {
 getAPI();
 
 
+
 // This function operates within the MovieInfo function for the onclick and targets a new page.
 function showMovie(imdbID) {
     localStorage.setItem("movieTag", imdbID)
@@ -25,30 +24,14 @@ function showMovie(imdbID) {
     
 }
 
-
 function movieInfo(movie) {
     return `
-        <div class="movie__card" onclick="showMovie('${movie.imdbID}')">
-            <div class="movie__card--container">
-                <p class="movie__card--poster"><img src="${movie.Poster}"></p>
-                <p class="movie__details"><b>${movie.Title}</b></p>
-                <p class="movie__details"><b>${movie.Year}</b></p>
-            </div>
-        </div>`
-        
-    }
-
-
-
-// Moving Background
-function moveBackground(event) {
-    const shapes = document.querySelectorAll(".shape")
-    const x = event.clientX * scaleFactor;
-    const y = event.clientY * scaleFactor;
-
-    for (let i = 0; i < shapes.length; ++i ) {
-        const isOdd = i % 2 !== 0;
-        const boolInt = isOdd ? -1 : 1;
-        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
-    }
+    <div class="movie__card" onclick="showMovie('${movie.imdbID}')">
+    <div class="movie__card--container">
+    <p class="movie__card--poster"><img src="${movie.Poster}"></p>
+    <p class="movie__details"><b>${movie.Title}</b></p>
+    <p class="movie__details"><b>${movie.Year}</b></p>
+    </div>
+    </div>`
+    
 }
